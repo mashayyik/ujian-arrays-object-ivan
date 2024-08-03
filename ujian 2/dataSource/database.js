116e39d6117f6023b2f67998f3198ca9029945ea
HLR <mashayyik@gmail.com>
2024-08-03 09:00:30 +0700

    review

---
+++ ujian 2/dataSource/database.js
@@ -0,0 +1,135 @@
+const videos = [
+    {
+        song: "Papi Chulo",
+        user: "Chikakiku",
+        viewers: 720,
+        rewards: [
+            ['Rocket', 11],
+            ['Chocolate', 13],
+            ['Ferrari', 1]
+        ],
+    },
+    {
+        song: "Saranghae",
+        user: "Lisa Stream",
+        viewers: 1400,
+        rewards: [
+            ['Diamond', 9],
+            ['Heart', 20],
+            ['Ferrari', 7],
+            ['Star', 30],
+        ]
+    },
+    {
+        song: "Mipan Zuzuzu",
+        user: "Mr Jack",
+        viewers: 32,
+        rewards: [
+            ['Diamond', 1],
+            ['Star', 5],
+        ]
+    },
+    {
+        song: "Savage Megan",
+        user: "12yes",
+        viewers: 980,
+        rewards: [
+            ['Star', 6],
+            ['Diamond', 3],
+            ['Rocket', 5],
+            ['Heart', 2],
+            ['Ferrari', 1],
+            ['Chocolate', 4],
+        ]
+    },
+    {
+        song: "Lottery",
+        user: "dj_cuap",
+        viewers: 1240,
+        rewards: [
+            ['Star', 30],
+            ['Heart', 10],
+            ['Chocolate', 2],
+            ['Ferrari', 4],
+        ]
+    },
+    {
+        song: "Roses",
+        user: "Dior777",
+        viewers: 560,
+        rewards: [
+            ['Rocket', 3],
+            ['Star', 10],
+            ['Diamond', 5],
+            ['Heart', 4],
+        ]
+    },
+    {
+        song: "Blinding Lights",
+        user: "StarBoy",
+        viewers: 2200,
+        rewards: [
+            ['Star', 50],
+            ['Diamond', 20],
+            ['Rocket', 15],
+            ['Heart', 25],
+        ]
+    },
+    {
+        song: "Levitating",
+        user: "DuaLover",
+        viewers: 1800,
+        rewards: [
+            ['Rocket', 10],
+            ['Chocolate', 5],
+            ['Ferrari', 3],
+            ['Heart', 15],
+        ]
+    },
+    {
+        song: "Dance Monkey",
+        user: "TonesAndI",
+        viewers: 2500,
+        rewards: [
+            ['Diamond', 30],
+            ['Star', 60],
+            ['Ferrari', 5],
+            ['Rocket', 20],
+        ]
+    },
+    {
+        song: "Peaches",
+        user: "JustinB",
+        viewers: 1500,
+        rewards: [
+            ['Star', 25],
+            ['Heart', 18],
+            ['Chocolate', 10],
+            ['Diamond', 8],
+        ]
+    },
+    {
+        song: "Butter",
+        user: "BTS_ARMY",
+        viewers: 3000,
+        rewards: [
+            ['Ferrari', 10],
+            ['Heart', 50],
+            ['Rocket', 25],
+            ['Diamond', 40],
+        ]
+    },
+];
+
+let listReward = [
+    {name: "Rocket", point: 100},
+    {name: "Chocolate", point: 200},
+    {name: "Ferrari", point: 500},
+    {name: "Heart", point: 50},
+    {name: "Diamond", point: 150},
+    {name: "Star", point: 25},
+];
+
+module.exports = {
+    videos, listReward
+}
