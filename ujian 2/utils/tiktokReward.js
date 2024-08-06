5512572e6bfb51e99afc5ec416e01e3c1bd6d482
Just-Naf <justivanelafif@gmail.com>
2024-08-06 19:57:20 +0700

    revisi ujian 2

--- ujian 2/utils/tiktokReward.js
+++ ujian 2/utils/tiktokReward.js
@@ -4,41 +4,40 @@ let result = [];
 videos.forEach((v) => {
 	let penonton = 0;
 	let point = 0;
-    penonton = v.viewers
-    let jamtayang = Math.floor((penonton * 52) / 3600)
-    
-	v.rewards.forEach((gift) => {
-        let monet = ""
-        let namaRewards = gift[0]
-        let totalRewards = gift[1]
-		let hadiahPerOrang = listReward.find(c => {
-            return c.name == namaRewards
-        });
-        
-        
-        if (jamtayang > 12) {
-            totalRewards * 750 
-            monet = "YES"
-        }else {totalRewards * 0 
-            monet = "NO"}
-            
-            let totalPoint = totalRewards * hadiahPerOrang.point
-            let duit = 0;
-            if (monet == "YES"){
-                duit = totalPoint * 750;
-        }
-                result.push({
-                            nama: v.user,
-                            judul: v.song,
-                            jmTyng: jamtayang,
-                            ads: totalPoint,
-                            cek: monet,
-                            uang: duit,
-                        });
-        
-	});
-    
+	penonton = v.viewers;
+	let duit = 0;
+	let totalPoint = 0;
+	let jamtayang = Math.floor((penonton * 52) / 3600);
+	let monet = "";
 
+	v.rewards.forEach((gift) => {
+		let namaRewards = gift[0];
+		let totalRewards = gift[1];
+		let hadiahPerOrang = listReward.find((c) => {
+			return c.name == namaRewards;
+		});
+
+		if (jamtayang > 12) {
+			totalRewards * 750;
+			monet = "YES";
+		} else {
+			totalRewards * 0;
+			monet = "NO";
+		}
+
+		if (monet == "YES") {
+			duit += totalRewards * hadiahPerOrang.point * 750;
+		}
+		totalPoint += totalRewards * hadiahPerOrang.point;
+	});
+	result.push({
+		nama: v.user,
+		judul: v.song,
+		jmTyng: jamtayang,
+		ads: totalPoint,
+		cek: monet,
+		uang: duit,
+	});
 });
 
-console.table(result)
+console.table(result);
